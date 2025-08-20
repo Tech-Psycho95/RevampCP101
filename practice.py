f3b70653f8dec0f4acbadae97a067b0fc9914f7e
SHIVAM <shivamsingh.smn@gmail.com>
2025-08-20 15:54:44 +0530

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,14 @@
+## to print cube of a number
+
+n = '9'
+print(int(n)*int(n)*int(n))
+
+## to print average of numbers
+
+a = "12"
+b = "15"
+c = "20"
+d = "18"
+e = "25"
+average = (int(a)+int(b)+int(c)+int(d)+int(e)) / 5
+print(average)
